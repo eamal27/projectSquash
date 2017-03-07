fbe3194fe3e14ae8185a27186cb845af6caf01c9
Hunter Thompson <hunter.thompson@uoit.net>
2017-03-07 14:04:38 -0500

    Started testing bash file

---
+++ RapidPrototype/main.cpp
@@ -0,0 +1,10 @@
+
+
+#include <iostream>
+#include <string>
+#include "System.h"
+#include "Transaction.h"
+#include "TicketInventory.h"
+#include "User.h"
+
+using namespace std;
--- tester.sh
+++ tester.sh
@@ -1,9 +1,26 @@
 #! /bin/bash
-file="$1"
+file="FrontEndRequirements/";
 
-for f in $file*; do
-    cp $f $f"_TerminalOutput.txt";
-    cp $f $f"_TransactionOutput.txt";
-    mv $f $f"_Input.txt";
-    #mv $f $f".txt";
+#test login first
+login=$file"login/login";
+input="_Input.txt";
+tranout="_TransactionOutput.txt";
+termout="_TerminalOutput.txt";
+passfail="pass";
+
+for num in {1..10}; do
+    ./RapidPrototype/a.out < $login$num$input &> output.txt;
+    if [ -z diff output.txt $login$num$termout]; then
+        passfail="pass";
+    else
+        passfail="fail";   
+    fi
+    echo "test login$num: $passfail" >> test.txt;
 done
+
+#for f in $file*; do
+#    cp $f $f"_TerminalOutput.txt";
+#    cp $f $f"_TransactionOutput.txt";
+#    mv $f $f"_Input.txt";
+    #mv $f $f".txt";
+#done
