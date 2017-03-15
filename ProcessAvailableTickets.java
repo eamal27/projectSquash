4a21a1399b34a88185f2cf85ed032651779428ec
Sohail Mohammed <SohailMohd@users.noreply.github.com>
2017-03-15 08:25:21 -0400

    added java class files, basic structure

---
+++ ProcessAvailableTickets.java
@@ -0,0 +1,23 @@
+
+
+public class ProcessAvailableTickets{
+	
+	public ArrayList<int> number_tickets;
+	public ArrayList<String> event_title;
+
+
+	public ProcessAvailableTickets(){
+		
+	}
+
+	public static ArrayList<int> determineTicketsLeft(){
+
+		return number_tickets;
+	}
+
+	public static ArrayList<String> determineEvent(){
+
+		return event_title;
+	}
+
+}
---
+++ UpdateUserAccounts.java
@@ -0,0 +1,25 @@
+
+
+public class UpdateUserAccounts{
+	
+	public UpdateUserAccounts(){
+
+	}
+
+	public void updateAmount(String username){
+
+	}
+
+	public void updateTickets(String event_title){
+
+	}
+
+	public void writeUsers(ArrayList<String> username_list){
+
+	}
+
+	public static void main(String args[]){
+
+	}
+}
+
