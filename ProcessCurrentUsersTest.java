dc84c1d34f202e26a759a2274a1f85461a20d52f
Sohail Mohammed <sohailm498@gmail.com>
2017-03-31 20:17:29 -0400

    added more statement coverage tests

--- ProcessCurrentUsersTest.java
+++ ProcessCurrentUsersTest.java
@@ -6,8 +6,25 @@ import org.junit.Test;
  */
 public class ProcessCurrentUsersTest extends TestCase {
 
+	String users_file = "CurrentUserAccounts.txt";
+	ProcessCurrentUsers currentUsers = new ProcessCurrentUsers();
+
     @Test
     public void testParseAccounts() throws Exception {
+
+    		/*buffer = new BufferedReader(new FileReader(users_file));
+			String line;
+			
+			while ((line = buffer.readLine()) != null){
+				if(!line.equals("END")){
+					String curr_username = line.substring(0, 15);
+					String curr_user_type = line.substring(16, 18);
+					String curr_user_amount = line.substring(19, 28);
+					float user_amount = Float.parseFloat(curr_user_amount);
+					this.accounts.add(new Account(curr_user_type,user_amount,curr_username));
+				}
+			}
+		*/
 
     }
 
--- TicketTest.java
+++ TicketTest.java
@@ -4,24 +4,37 @@ import org.junit.Test;
 
 public class TicketTest extends TestCase {
 
+    public String expected_event = "SomeEvent";
+    public String expected_seller = "caleb";
+    public int expected_tickets = 10;
+    public float expected_ticketPrice = 9.99f;
+    public Ticket newTicket = new Ticket(expected_event, expected_seller,
+     expected_tickets, expected_ticketPrice);     
+
     @Test
     public void testGetEventName() throws Exception {
-        assertEquals(1,1);
+        String eventTitle = newTicket.getEventName(); 
+
+        assertEquals(expected_event, eventTitle);
     }
 
     @Test
     public void testGetSellerUsername() throws Exception {
-        assertEquals(1,1);
+        String seller_username = newTicket.getSellerUsername();
+
+        assertEquals(expected_seller, seller_username);
     }
 
     @Test
     public void testGetTicketCount() throws Exception {
-        assertEquals(1,1);
+        int num_tickets = newTicket.getTicketCount(); 
+        assertEquals(expected_tickets, num_tickets);
     }
 
     @Test
     public void testGetTicketPrice() throws Exception {
-        assertEquals(1,1);
+        float ticket_price = newTicket.getTicketPrice();
+        assertEquals(expected_ticketPrice, ticket_price);
     }
 
 }
